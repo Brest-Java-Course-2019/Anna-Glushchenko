10eac9c804c975970990b7d27e8c07fcff48c8fb
AnnaGlshchnk <anna.glshchnk@mail.ru>
2019-01-19 17:22:37 +0300

    add property

--- Delivery_Cost/src/main/java/com/epam/brest/cources/DeliveryCost.java
+++ Delivery_Cost/src/main/java/com/epam/brest/cources/DeliveryCost.java
@@ -1,20 +1,60 @@
 package com.epam.brest.cources;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class DeliveryCost {
 
     public static void main(String[] args) {
+
+        calculate();
+    }
+
+    private static void calculate() {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Weight: ");
-        double weight = in.nextFloat();
-        System.out.println("Distance: ");
-        double distance = in.nextFloat();
-        double price = 1.23;
-        double cost = weight*distance*price;
-        System.out.printf("Delivery cost :  %.2f", cost);
+        FileInputStream fis;
+        Properties property = new Properties();
 
+        try {
+            fis = new FileInputStream("/home/anna/Documents/Projects/Anna-Glushchenko/Delivery_Cost/src/main/resources/prices.properties");
+            property.load(fis);
+
+            double min = Double.parseDouble(property.getProperty("min.weight"));
+            double medium = Double.parseDouble(property.getProperty("medium.weight"));
+            double max = Double.parseDouble(property.getProperty("max.weight"));
+
+            System.out.println("Weight: ");
+            double weight = in.nextFloat();
+            if (weight <= 0) {
+                System.out.print("This data must be greater than 0");
+                return;
+            }
+            System.out.println("Distance: ");
+            double distance = in.nextFloat();
+            if (distance <= 0) {
+                System.out.print("this data must be greater than 0");
+                return;
+            }
+
+            if (weight < 5) {
+                double cost = weight * distance * min;
+                System.out.printf("Delivery cost: %.2f", cost);
+            }
+            if (weight >= 5 && weight < 10) {
+                double cost = weight * distance * medium;
+                System.out.printf("Delivery cost: %.2f", cost);
+            }
+            if (weight >= 10) {
+                double cost = weight * distance * max;
+                System.out.printf("Delivery cost: %.2f", cost);
+            }
+
+        } catch (IOException e) {
+            System.err.println("File not found!");
+        }
     }
 
 }
